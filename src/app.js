3823cb62d513cd0c524801ee7b2568b1574170c1
Rishabh Tiwari <pt64632@gmail.com>
2026-06-10 23:24:35 +0530

    utils made

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'cors';
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app = express();
@@ -12,4 +12,4 @@ app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended: true,limit:'16kb'}));
 app.use(express.static('public'));
 app.use(cookieParser())
-export {app};
+export {app}

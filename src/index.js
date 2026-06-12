4e5bf071d5f40e32fdc27ffbc60fdda96cf2cc4e
Rishabh Tiwari <pt64632@gmail.com>
2026-06-12 23:35:06 +0530

    cloudiary handled

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-import {app} from "./app.js";
 
 dotenv.config({
-    path : './.env'
+  path: "./.env",
 });
 
-connectDB().then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
     });
-})
-.catch((error) => {
-    console.log("Failed to connect to the database:", error);
-});
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  });
+
+  console.log("INDEX ENV CHECK");
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,11 +2,9 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log("FILE RECEIVED:", file);
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    console.log("FILENAME:", file.originalname);
     cb(null, file.originalname);
   },
 });
